b12e5a77383df3ea634c16ea4c6579983e7ae011
tuboi <t_tuboi@aquria.co.jp>
2010-03-03 16:27:05 +0900

    サイズ調整 メニュー実装 通知アイコン

--- TTSwTask/resource.h
+++ TTSwTask/resource.h
@@ -3,9 +3,16 @@
 // Used by TTSwTask.rc
 //
 #define IDC_MYICON                      2
+#define IDI_TTSWTASK                    5
 #define IDD_TTSWTASK_DIALOG             102
-#define IDI_TTSWTASK                    107
 #define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
+#define ID_32771                        32771
+#define ID_UPDATE_CONFIG                32772
+#define ID_32773                        32773
+#define ID_APP_QUIT                     32774
+#define ID_32775                        32775
+#define ID_VIEW_LIST                    32776
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -13,8 +20,8 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32777
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
