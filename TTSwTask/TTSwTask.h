ce13791876f51fb0f2bcac5a4802d304ffeae93b
tetu-dc5 <tetu.dc5@gmail.com>
2010-09-18 23:17:19 +0900

    CEditIniFileクラスを使って、設定を編集するように書き換えた

--- TTSwTask/TTSwTask.h
+++ TTSwTask/TTSwTask.h
@@ -4,14 +4,16 @@
 #include "ImageList.h"
 #include "WndList.h"
 #include "launcher.h"
+#include "EditIniFile.h"
 
 #define		TMP_BUF_SIZE	(1024)
 
-extern HINSTANCE	g_hInst;
-extern HWND			g_hWnd;
-extern CImageList*	g_ImageList;
-extern CWndList*	g_WndList;
-extern CLauncher*	g_Launcher;
-extern LPCTSTR		g_IniPath;
+extern HINSTANCE		g_hInst;
+extern HWND				g_hWnd;
+extern CImageList*		g_ImageList;
+extern CWndList*		g_WndList;
+extern CLauncher*		g_Launcher;
+extern CEditIniFile*	g_EditIni;
+extern LPCTSTR			g_IniPath;
 
 LPTSTR CopyString(LPCTSTR src);
