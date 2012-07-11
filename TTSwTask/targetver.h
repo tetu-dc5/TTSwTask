ed91bdef6f42c7f3e5bee9af38dba43c04ca5bec
tetu.dc5 <tetu.dc5@gmail.com>
2012-07-11 22:00:11 +0900

    なんとなくAero対応が出来た
    POPUPWINDOWだとなぜかAero化した時に抜き状態になってしまうので、
    Aeroの時はOVERWRAPに変更・・・キャプション消さないかなぁ～

--- TTSwTask/targetver.h
+++ TTSwTask/targetver.h
@@ -1,17 +1,3 @@
 #pragma once
 
-#ifndef WINVER
-#define WINVER 0x0510
-#endif
-
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0510
-#endif
-
-#ifndef _WIN32_WINDOWS
-#define _WIN32_WINDOWS 0x0410
-#endif
-
-#ifndef _WIN32_IE
-#define _WIN32_IE 0x0700
-#endif
+#include <SDKDDKVer.h>
